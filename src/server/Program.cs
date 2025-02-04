a8cf00a13ef68e76e1470c8edd8cf19466db5f64
shead0shead <accounts@egorkonovalov.ru>
2025-02-04 21:16:22 +0300

    Chore: Update version in start menu

--- src/server/Program.cs
+++ src/server/Program.cs
@@ -257,7 +257,7 @@ class Menu
         "Tool    :: Remote Powershell",
         "Author  :: Egor Konovalov (shead0shead)",
         "GitHub  :: github.com/shead0shead/remote-powershell",
-        "Version :: 1.2"
+        "Version :: 1.3"
     };
 
     protected string[] menu =
